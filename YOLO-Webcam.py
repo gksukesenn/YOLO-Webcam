3a1f9cca07ea99d9ccbb477e9c2211a8854ced47
Göksu Kesen <gksukesenn@gmail.com>
2023-12-16 14:00:11 +0300

    for fancy rectangles we used cvzone instead of using opencv

--- YOLO-Webcam.py
+++ YOLO-Webcam.py
@@ -16,8 +16,10 @@ while True:
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)   # printte tensor olarak değil integer olarak sayıları istiyoruz.
-            print(x1, y1, x2, y2)
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,255), 3)
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,255), 3)
+
+            w, h = x2-x1, y2-y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
 
     cv2.imshow("İmage", img)
     cv2.waitKey(1)
