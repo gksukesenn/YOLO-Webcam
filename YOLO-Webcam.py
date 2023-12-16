d29cacace8cf10a3731481ef1b439e5658ee07ce
Göksu Kesen <gksukesenn@gmail.com>
2023-12-16 13:49:00 +0300

    Packages are downloaded and defined objects were shown in rectangles

---
+++ YOLO-Webcam.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+model = YOLO("../YOLO-Weights/yolov8n.pt")
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)   # printte tensor olarak değil integer olarak sayıları istiyoruz.
+            print(x1, y1, x2, y2)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,255), 3)
+
+    cv2.imshow("İmage", img)
+    cv2.waitKey(1)
+
